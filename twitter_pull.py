9987b0a165078eee77bbf0e91495d8b30378bcf3
kaushilk <kkash321@gmail.com>
2019-04-03 21:27:54 -0400

    2nd day

--- twitter_pull.py
+++ twitter_pull.py
@@ -1,16 +1,23 @@
 import requests
 from bs4 import BeautifulSoup
 
-news = "CNN"
+class TwitterStory:
+    def __init__(self, name, text, time):
+        self.name = name
+        self.text = text
+        self.time = time
 
-source = requests.get('https://twitter.com/' + str(news))
-soup = BeautifulSoup(source.text, 'html.parser')
-list = soup.find_all(class_ = 'content')
+def pull_stories(x):
+    source = requests.get('https://twitter.com/' + str(x))
+    soup = BeautifulSoup(source.text, 'html.parser')
+    list = soup.find_all(class_ = 'content')
 
-n = 1
-for item in list:
-    print("\nCNN Story #", n , "\n------------")
-    text = item.p.contents[0]
-    time = item.small.a['title']
-    print(time, "\n", text, "\n------------")
-    n += 1
+    n = 1
+    for item in list:
+        print("\nCNN Story #", n , "\n------------")
+        text = item.p.contents[0]
+        time = item.small.a['title']
+        print(time, "\n", text, "\n------------")
+        n += 1
+
+pull_stories("FoxNews")
