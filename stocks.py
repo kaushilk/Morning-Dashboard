9f880d4a974ca559fda9b4c2c42e57f22bc91cf4
kaushilk <kkash321@gmail.com>
2019-04-16 20:43:15 -0400

    master

--- stocks.py
+++ stocks.py
@@ -1,5 +1,26 @@
-from googlefinance.get import get_code
+import requests
+from bs4 import BeautifulSoup
 
-get_code()
-get_code('NASDAQ')
-get_code('NYSE')
+def pull_finance(x):
+    b = open("feeed.txt", "w")
+    source = requests.get('https://finance.yahoo.com/' + str(x))
+    soup = BeautifulSoup(source.text, 'html.parser')
+    list = soup.find_all('td')
+    print(list[0].next_element.next_element.next_element.next_element.next_element.next_element)
+    for tag in list:
+        if tag.string == None:
+            pass
+        else:
+            
+            b.write("\n")
+            b.write(tag.string)
+            b.write("\n")
+pull_finance("most-active")
+
+
+
+
+
+# from yahoo_fin import stock_info as si
+#
+# print(si.get_live_price("amzn"))
