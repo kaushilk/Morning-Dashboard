a49e03f3215f70369d54ec083f90f611208888a4
kaushilk <kkash321@gmail.com>
2019-04-17 20:39:57 -0400

    master

--- stocks.py
+++ stocks.py
@@ -1,26 +1,36 @@
-import requests
-from bs4 import BeautifulSoup
 
-def pull_finance(x):
-    b = open("feeed.txt", "w")
-    source = requests.get('https://finance.yahoo.com/' + str(x))
-    soup = BeautifulSoup(source.text, 'html.parser')
-    list = soup.find_all('td')
-    print(list[0].next_element.next_element.next_element.next_element.next_element.next_element)
-    for tag in list:
-        if tag.string == None:
-            pass
-        else:
-            
-            b.write("\n")
-            b.write(tag.string)
-            b.write("\n")
-pull_finance("most-active")
+from yahoo_fin import stock_info as si
+
+print(si.get_live_price("aapl"))
 
 
 
 
 
-# from yahoo_fin import stock_info as si
+
+
+
+
+
+
+
+
+# import requests
+# from bs4 import BeautifulSoup
 #
-# print(si.get_live_price("amzn"))
+# def pull_finance(x):
+#     b = open("feeed.txt", "w")
+#     source = requests.get('https://finance.yahoo.com/' + str(x))
+#     soup = BeautifulSoup(source.text, 'html.parser')
+#     list = soup.find_all('td')
+#     print(list[0].next_element.next_element.next_element.next_element.next_element.next_element)
+#     for tag in list:
+#         if tag.string == None:
+#             pass
+#         else:
+#             print(tag)
+#             print("\n--------\n")
+#             b.write("\n")
+#             b.write(tag.string)
+#             b.write("\n")
+# pull_finance("most-active")
