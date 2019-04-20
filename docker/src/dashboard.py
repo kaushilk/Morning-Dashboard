75ea0de6b0f39069fed63ae675c1c01e5e8ef847
kaushilk <kkash321@gmail.com>
2019-04-20 11:55:28 -0400

    master

--- docker/src/dashboard.py
+++ docker/src/dashboard.py
@@ -1,6 +1,7 @@
 from twitter_pull import pull_stories, stories
 from twitter_pull import TwitterStory
 import datetime
+from to_do import projects
 
 time = datetime.datetime.now()
 day = str(time.day)[1]
@@ -16,8 +17,11 @@ pull_stories("CNN")
 print(time, "\n----------")
 for x in stories:
     if x.name == "CNN" and x.time[-10] == day:
-        TwitterStory.display_story(x)
+        # TwitterStory.display_story(x)
         TwitterStory.write_story(x, f)
     else:
-        TwitterStory.display_story(x)
+        # TwitterStory.display_story(x)
         TwitterStory.write_story(x, f)
+
+y = open("feed/feed.txt", "r")
+print(y.read())
